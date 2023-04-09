fee500469dca89a2eda4812041419ce27f7d46c0
Gokul2003g <gokul2003g@gmail.com>
2023-04-09 17:28:36 +0530

    created pop up form for registring team

---
+++ src/components/events/RegisterMembers.jsx
@@ -0,0 +1,99 @@
+import { Fragment, useState } from "react";
+import {
+    Button,
+    Dialog,
+    DialogHeader,
+    DialogBody,
+    DialogFooter,
+    Input,
+    Checkbox,
+    Typography,
+} from "@material-tailwind/react";
+
+export default function ConfirmPayment({ membercount, buttonLabel }) {
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => {
+        const auth = JSON.parse(localStorage.getItem('isLoggedIn'));
+
+        if(0) {
+            alert("Login Required to view this. Redirecting to Login Page");
+            return window.location.href = "/login";
+        }
+        setOpen(!open)
+    };
+
+    const handleMembers = () => {
+        // send team members to backend
+    };
+
+    const numOrders = 4; // number of orders to render
+    const orders = [...Array(numOrders).keys()]; // generate an array of order numbers
+
+    
+    const orderElements = orders.map(orderNum => (
+      <div key={orderNum}>
+        <label>Enter Member details</label>
+        <Input size="lg" label={`Member ${orderNum}`} value={orderNum + 1} />
+      </div>
+    ));
+    
+    return (
+            <Fragment>
+                <Button className="filled text-backgroundColor bg-khaki justify-center w-full text-lg" onClick={handleOpen} variant="filled">
+                    {buttonLabel}
+                </Button>
+                <Dialog open={open} size={"md"} handler={handleOpen} className="bg-babyPowder">
+                    <DialogHeader>Register Members for Events</DialogHeader>
+                    <DialogBody divider>
+                        <form className="mt-8 mb-2 w-3/5 ml-auto mr-auto max-w-screen-lg">
+                            <div className="mb-4 flex text-center flex-col gap-2">
+                                {orderElements}
+                                <label>Pincode</label>
+                                <Input size="lg" label="Pincode" type={"number"} maxLength="6" required />
+                            </div>
+                        </form>
+                    </DialogBody>
+                    <DialogFooter className="w-fit ml-auto mr-auto">
+                        <form className="block">
+                            <div>
+                                <Checkbox
+                                    required
+                                    label={
+                                        (
+                                            <Typography
+                                                variant="small"
+                                                color="gray"
+                                                className="flex items-center font-normal"
+                                            >
+                                                I agree the
+                                                <a
+                                                    href="/terms-and-conditions"
+                                                    className="font-medium transition-colors hover:text-blue-500"
+                                                >
+                                                    &nbsp;Terms and Conditions
+                                                </a>
+                                            </Typography>
+                                        )
+                                    }
+                                    containerProps={{ className: "-ml-2.5" }}
+                                />
+                            </div>
+                            <div className="mt-4">
+                                <Button
+                                    variant="text"
+                                    color="red"
+                                    onClick={handleOpen}
+                                    className="mr-4"
+                                >
+                                    <span>Cancel</span>
+                                </Button>
+                                <Button variant="filled" className="bg-backgroundColor text-babyPowder" onClick={handleMembers}>
+                                    <span>Register Team</span>
+                                </Button>
+                            </div>
+                        </form>
+                    </DialogFooter>
+                </Dialog>
+            </Fragment>
+    );
+}
