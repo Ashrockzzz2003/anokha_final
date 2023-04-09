4f0e02369b2acb6ff3a2f7b6aad6f0d57f378302
SuperSecureHuman <supersecurehuman@gmail.com>
2023-04-09 17:40:19 +0530

    SearchBar Color - Onclick changes

--- src/components/events/Searchbar.jsx
+++ src/components/events/Searchbar.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Searchbar = ({ keyword, onChange }) => {
-  // Define the style for the search bar
+  const [isClicked, setIsClicked] = useState(false);
+
   const barStyle = {
     width: "80%",
     boxShadow: "0 16px 64px 0 rgba(255, 255, 255, .05)",
@@ -11,30 +12,32 @@ const Searchbar = ({ keyword, onChange }) => {
     outline: "none",
     fontSize: "18px",
     fontWeight: "500",
-    background: "rgba(0,0,0,0)",
+    background: isClicked ? "rgba(255,255,255,1)" : "rgba(0,0,0,0)",
     padding: "14px 20px",
     margin: "10px auto",
     display: "block",
     textAlign: "center",
     transition: "0.3s",
+    color: isClicked ? "black" : "white",
+  };
 
-    // Add styles for the :focus pseudo-class to add a black border and highlight effect on click
-    ':focus': {
-      border: '2px solid black',
-      outline: 'none',
-      boxShadow: '0 0 10px rgba(0,0,0,0.3)',
-      background: 'rgba(255, 255, 255, 0.5)',
-    }
+  const handleClick = () => {
+    setIsClicked(!isClicked);
+  };
+
+  const handleBlur = () => {
+    setIsClicked(false);
   };
 
   return (
-    // Render the search bar as an input element with the defined style
     <input
       style={barStyle}
       key="search-bar"
       value={keyword}
       placeholder="Search Here"
       className="text-babyPowder"
+      onClick={handleClick}
+      onBlur={handleBlur}
       onChange={(e) => onChange(e.target.value)}
     />
   );
