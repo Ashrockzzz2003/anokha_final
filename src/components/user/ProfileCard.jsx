2eb3a4f5175a627a71bc0e64e7ad0a6ea0a53778
Gokul2003g <gokul2003g@gmail.com>
2023-04-10 08:57:38 +0530

    added qr code package

--- src/components/user/ProfileCard.jsx
+++ src/components/user/ProfileCard.jsx
@@ -4,14 +4,16 @@ import {
   CardBody,
   Typography,
 } from "@material-tailwind/react";
+import QRCode from "react-qr-code";
 
 
-export default function ProfileCard({ fullName, userEmail }) {
+export default function ProfileCard({ fullName, userEmail, qrlink }) {
   return (
     <div className="py-32">
       <Card className="w-fit ml-auto mr-auto">
         <CardHeader floated={false} className="flex flex-col lg:flex-row justify-evenly">
           <img className="w-72 ml-auto mr-auto lg:ml-0 lg:mr-0" src="https://www.dummies.com/wp-content/uploads/324172.image0.jpg" alt="dummy-qr" />
+          <QRCode title="profile-qr" value={qrlink} size={50} level="L" />
         </CardHeader>
         <CardBody className="text-center">
           <Typography variant="h4" color="blue-gray" className="mb-2">
