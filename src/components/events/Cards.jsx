9d73eb1d854f91b908c38fae2a89ea4c74935154
Gokul2003g <gokul2003g@gmail.com>
2023-04-11 15:33:55 +0530

    added bookmark

--- src/components/events/Cards.jsx
+++ src/components/events/Cards.jsx
@@ -16,35 +16,53 @@ import {
   TvIcon,
   FireIcon,
 } from "@heroicons/react/24/solid";
- 
-export default function Example({ key, title, imageUrl, description, tags, buttonLabel, linkTo }) {
+import { useState } from "react";
+
+export default function Example({
+  key,
+  title,
+  imageUrl,
+  description,
+  tags,
+  buttonLabel,
+  linkTo,
+}) {
+  const [isBookmarked, setIsBookmarked] = useState(false);
+  const handleBookmarkClick = () => {
+    setIsBookmarked(!isBookmarked);
+  };
+
   return (
-    <Card className="w-full max-w-[26rem] shadow-lg grayscale duration-500 hover:grayscale-0">
+    <Card className="w-full max-w-[26rem] shadow-lg md:grayscale duration-500 hover:grayscale-0">
       <CardHeader floated={false} color="blue-gray">
-        <img
-          src={imageUrl}
-          alt="ui/ux review check"
-        />
+        <img src={imageUrl} alt="ui/ux review check" />
         <div className="to-bg-black-10 absolute inset-0 h-full w-full bg-gradient-to-tr from-transparent via-transparent to-black/60 " />
+        {isBookmarked ? 
         <IconButton
+        onClick={handleBookmarkClick}
+        size="sm"
+        color="red"
+        variant="text"
+        className="!absolute top-4 right-4 duration-0 rounded-full">
+        <HeartIcon className="h-6 w-6" />
+      </IconButton>
+      : 
+      <IconButton
+          onClick={handleBookmarkClick}
           size="sm"
-          color="red"
+          color="white"
           variant="text"
-          className="!absolute top-4 right-4 rounded-full"
-        >
+          className="!absolute top-4 right-4 duration-0 rounded-full">
           <HeartIcon className="h-6 w-6" />
-        </IconButton>
+        </IconButton>}
       </CardHeader>
       <CardBody>
         <div className="mb-3 flex items-center justify-between">
           <Typography variant="h5" color="blue-gray" className="font-medium">
             {title}
           </Typography>
-          
         </div>
-        <Typography color="gray">
-          {description}
-        </Typography>
+        <Typography color="gray">{description}</Typography>
         <div className="group mt-8 inline-flex flex-wrap items-center gap-3">
           <Tooltip content="$129 per night">
             <span className="cursor-pointer rounded-full border border-blue-500/5 bg-blue-500/5 p-3 text-blue-500 transition-colors hover:border-blue-500/10 hover:bg-blue-500/10 hover:!opacity-100 group-hover:opacity-70">
@@ -80,15 +98,14 @@ export default function Example({ key, title, imageUrl, description, tags, butto
       </CardBody>
       <CardFooter className="pt-3">
         <a href={linkTo}>
-        <Button size="lg" fullWidth={true}>
-          {buttonLabel}
-        </Button>
+          <Button size="lg" fullWidth={true}>
+            {buttonLabel}
+          </Button>
         </a>
       </CardFooter>
     </Card>
   );
 }
-
 
 // import { Button, Typography } from '@material-tailwind/react';
 // import React from 'react';
