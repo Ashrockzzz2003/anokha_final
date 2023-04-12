c72090a26b1d5d9e5a0ac112e0c23361a5a69860
Gokul2003g <gokul2003g@gmail.com>
2023-04-12 13:37:35 +0530

    passport layout

--- src/components/events/AmritaPassport.jsx
+++ src/components/events/AmritaPassport.jsx
@@ -1,63 +1,58 @@
-import { Button, Typography } from "@material-tailwind/react";
+import {Button, Typography} from "@material-tailwind/react";
 import React from "react";
 import eventide from "../utils/eventide.png";
 import RegisterMembers from "../events/RegisterMembers";
 import ConfirmPaymentPage from "../user/ConfirmPaymentPage.jsx";
+import { useAuth } from "../../auth/useAuth";
+import { useParams } from "react-router-dom";
+import secureLocalStorage from "react-secure-storage";
 
 const EventLanding = () => {
-  const data = {
-    "orderID": "order_DBJOWzybf0sJbb",
-    "amount": 50000,
-    "eventName": "Eventide",
-    "fullName": "Vira Manohar aka LEGEND",
-    "email": "vira_manohar_legend@cb.students.amrita.edu",
-  }
-
   
-  /*
-        "eventId": 1,
-        "eventName": "EVENT0",
-        "eventOrWorkshop": 1,
-        "groupOrIndividual": 1,
-        "maxCount": 1,
-        "description": "DESC0",
-        "url": "https://play-lh.googleusercontent.com/VojafVZNddI6JvdDGWFrRmxc-prrcInL2AuBymsqGoeXjT4f9sv7KnetB-v3iLxk_Koi",
-        "userEmail": "managerEmail1@gmail.com",
-        "date": "24-Mar-2023",
-        "eventTime": "08:14:57",
-        "venue": "VENUE0",
-        "fees": 100,
-        "totalNumberOfSeats": 100,
-        "noOfRegistrations": 0,
-        "timeStamp": "2023-04-04T10:51:47.000Z",
-        "refundable": 0,
-        "departmentAbbr": "DEP0"
-  */
+  const { moveToTransaction } = useAuth();
+  const { eventId } = useParams();
+
+  // Handle Login
+  const handleBuyPassport = (e) => {
+    e.preventDefault();
+    if (!JSON.parse(secureLocalStorage.getItem("token"))) {
+      alert("Please Login to Buy Passport.");
+      window.location.href = "/login";
+      return;
+    }
+    moveToTransaction(parseInt(eventId));
+  };
+  
 
   return (
     <div className="w-full block items-center justify-center lg:flex lg:justify-center pb-32 pt-32">
       <div className="w-auto lg:pr-12">
-        <img src={eventide} className="w-72 rounded-lg ml-auto mr-auto" alt="Event Pix" />
+        <img
+          src={eventide}
+          className="w-72 rounded-lg ml-auto mr-auto"
+          alt="Event Pix"
+        />
         <div className="mt-4 w-fit ml-auto mr-auto">
-            <Button
-              variant="filled"
-              className="bg-khaki text-backgroundColor"
-              onClick={() => {}}
-            >
-              <span>Buy Passport</span>
-            </Button>
-          </div>
-
+          <Button
+            variant="filled"
+            className="bg-khaki text-backgroundColor"
+            onClick={handleBuyPassport}>
+            <span>Buy Passport</span>
+          </Button>
+        </div>
       </div>
       <div className="m-16 mt-24 lg:pl-24 lg:ml-16 lg:w-1/2">
         <Typography variant="h1" className="mb-4 text-khaki text-left">
-dUMMY data
+          dUMMY data
         </Typography>
         <Typography variant="h4" className="mb-16 text-khaki text-left">
           Tagline
         </Typography>
-        <Typography variant="paragraph" className="mb-16 text-babyPowder text-left">
-Dummy content        </Typography>
+        <Typography
+          variant="paragraph"
+          className="mb-16 text-babyPowder text-left">
+          Dummy content{" "}
+        </Typography>
       </div>
     </div>
   );
