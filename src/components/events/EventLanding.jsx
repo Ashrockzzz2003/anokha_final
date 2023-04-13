977bc045581610b8e3bd2fba2d9613e4d99809f8
Ashrockzzz2003 <ashrockzzz2003@gmail.com>
2023-04-13 08:06:21 +0530

    event Page

--- src/components/events/EventLanding.jsx
+++ src/components/events/EventLanding.jsx
@@ -1,21 +1,29 @@
 import { Typography, Button } from "@material-tailwind/react";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import RegisterMembers from "../events/RegisterMembers";
 import { useAuth } from "../../auth/useAuth";
 import { useParams } from 'react-router-dom'
 import secureLocalStorage from "react-secure-storage";
-import Markdown from 'react-markdown';
-
-import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const EventLanding = () => {
-
-  const { moveToTransaction } = useAuth();
   const { eventId } = useParams();
 
   const [isAmritaCBE, setIsAmritaCBE] = useState(secureLocalStorage.getItem("isAmritaCBE"));
   const [hasActivePassport, setHasActivePassport] = useState(parseInt(secureLocalStorage.getItem("hasActivePassport")));
+
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    fetch('https://anokha.amrita.edu/api/userWeb/events/all')
+      .then(response => response.json())
+      .then(data => setEvents(data));
+  }, []);
+
+  // store events in secureLocalStorage
+  secureLocalStorage.setItem("events", JSON.stringify(events));
+
+  const { moveToTransaction } = useAuth();
 
 
   // Handle Login
@@ -35,8 +43,6 @@ const EventLanding = () => {
     moveToTransaction(isAmritaCBE, parseInt(eventId), false);
   };
 
-  const [events] = useState(JSON.parse(secureLocalStorage.getItem("events")));
-
 
   // "eventId": 1,
   // "eventName": "EVENT0",
@@ -55,48 +61,64 @@ const EventLanding = () => {
   // "timeStamp": "2023-04-04T10:51:47.000Z",
   // "refundable": 0,
   // "departmentAbbr": "DEP0"
-  
-
-
-  // get Event by ID
-  const [event] = useState(
-    events.filter((event) => event.eventId === parseInt(eventId)
-    ));
-
-  const isGroupEvent = event[0].groupOrIndividual === 1;
 
   return (
-    <div className="w-full block items-center justify-center lg:flex lg:justify-center pb-32 pt-40">
-      <div className="w-auto lg:pr-12">
-        <img src={event[0].url} className="w-72 rounded-lg ml-auto mr-auto" alt="Event Pix" />
-        {isGroupEvent ?
-          <div className="mt-4 w-fit ml-auto mr-auto">
-            <RegisterMembers membercount={4} amount={500} buttonLabel={"Register with Members"} />
-          </div>
-          :
-          <div className="mt-4 w-fit pt-4 ml-auto mr-auto">
-            <Button
-              variant="filled"
-              className="bg-khaki text-backgroundColor"
-              size="lg"
-              onClick={handleRegsiter}
-            >
-              <span className="text-lg">Register</span>
-            </Button>
-          </div>
-        }
+    <div className="mt-16">
+      <Typography
+        variant="h1"
+        className="mb-2 pt-8 text-4xl md:text-6xl  lg:text-8xl text-lime-50 text-center"
+      >
+        About the Event
+      </Typography>
+      <div className="flex justify-center">
+        <div className="w-4/5">
+          <div className="flex flex-wrap justify-center gap-8 items-center pt-5">
+            {events.length ? events.map((event) => (
+              <div>
+                {event.eventId === parseInt(eventId) ?
+                  <div className="w-full block items-center justify-center lg:flex lg:justify-center pb-32 pt-8">
+                    <div className="w-auto lg:pr-12">
+                      <img src={event.url} className="w-72 rounded-lg ml-auto mr-auto" alt="Event Pix" />
+                      {event.groupOrIndividual === 1 ?
+                        <div className="mt-4 w-fit ml-auto mr-auto">
+                          <RegisterMembers membercount={4} amount={500} buttonLabel={"Register with Members"} />
+                        </div>
+                        :
+                        <div className="mt-4 w-fit pt-4 ml-auto mr-auto">
+                          <Button
+                            variant="filled"
+                            className="bg-khaki text-backgroundColor"
+                            size="lg"
+                            onClick={handleRegsiter}
+                          >
+                            <span className="text-lg">Register</span>
+                          </Button>
+                        </div>
+                      }
 
-      </div>
-      <div className="m-16 mt-24 lg:pl-24 lg:ml-16 lg:w-1/2">
-        <Typography variant="h1" className="mb-4 font-title uppercase tracking-wider text-khaki text-left">
-          {event[0].eventName}
-        </Typography>
-        {/* <Typography variant="h4" className="mb-16 text-khaki text-left">
-          Tagline
-        </Typography> */}
-        <Typography className="mb-16 text-babyPowder text-left">
-          {event[0].description}
-        </Typography>
+                    </div>
+                    <div className="m-16 mt-24 lg:pl-24 lg:ml-16 lg:w-1/2">
+                      <Typography variant="h1" className="mb-4 font-title uppercase tracking-wider text-khaki text-left">
+                        {event.eventName}
+                      </Typography>
+                      {/* <Typography variant="h4" className="mb-16 text-khaki text-left">
+                  Tagline
+                </Typography> */}
+                      <Typography className="mb-16 text-babyPowder text-left">
+                        {event.description}
+                      </Typography>
+                    </div>
+                  </div>
+                  : null}
+              </div>
+            )) : <Typography
+              variant="h6"
+              className="mb-2 pt-8 text-lime-50 text-center"
+            >
+              No events found
+            </Typography>}
+          </div>
+        </div>
       </div>
     </div>
   );
